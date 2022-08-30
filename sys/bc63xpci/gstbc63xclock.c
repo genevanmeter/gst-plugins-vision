7f60350a9b42810d44984bc925d1d2f52d38adbf
Gene VanMeter <gvanmeter@proton.me>
2022-08-30 14:13:22 -0400

    Copying GstPtpClock as a template

--- sys/bc63xpci/gstbc63xclock.c
+++ sys/bc63xpci/gstbc63xclock.c
@@ -1,1 +1,148 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "gstbc63xclock.h"
+
+#ifdef G_OS_WIN32
+#  define WIN32_LEAN_AND_MEAN   /* prevents from including too many things */
+#  include <windows.h>          /* Need for BC637PCI.h on Windows */
+#  undef WIN32_LEAN_AND_MEAN
+#  ifndef EWOULDBLOCK
+#  define EWOULDBLOCK EAGAIN    /* This is just to placate gcc */
+#  endif
+#endif /* G_OS_WIN32 */
+#include "BC637PCI.h"
+
+enum
+{
+  PROP_0,
+  PROP_DEVICE_NUMBER
+};
+
+static gboolean bc63xOpened = FALSE;
+
+
+struct _GstBc63xClockPrivate
+{
+  guint device_number;
+  GstClock *domain_clock;
+};
+
+
+#define gst_bc63x_clock_parent_class parent_class
+G_DEFINE_TYPE_WITH_PRIVATE (GstBc63xClock, gst_bc63x_clock, GST_TYPE_SYSTEM_CLOCK);
+
+static void gst_bc63x_clock_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec);
+static void gst_bc63x_clock_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec);
+static void gst_bc63x_clock_finalize (GObject * object);
+
+
+
+static void
+gst_bc63x_clock_class_init (GstBc63xClockClass * klass)
+{
+  GObjectClass *gobject_class;
+  GstClockClass *clock_class;
+
+  gobject_class = G_OBJECT_CLASS (klass);
+  clock_class = GST_CLOCK_CLASS (klass);
+
+  gobject_class->finalize = gst_bc63x_clock_finalize;
+  gobject_class->get_property = gst_bc63x_clock_get_property;
+  gobject_class->set_property = gst_bc63x_clock_set_property;
+
+  g_object_class_install_property (gobject_class, PROP_DEVICE_NUMBER,
+      g_param_spec_uint ("device-number", "Device number",
+          "Timing card instance to use", 0, G_MAXUINT, 0,
+          (GParamFlags) (G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS |
+              G_PARAM_CONSTRUCT)));
+
+  //clock_class->get_internal_time = gst_ptp_clock_get_internal_time;
+
+}
+
+static void
+gst_bc63x_clock_init (GstBc63xClock * self)
+{
+
+}
+
+static void
+gst_bc63x_clock_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GstBc63xClock *self = GST_BC63X_CLOCK(object);
+
+  switch (prop_id) {
+    case PROP_DEVICE_NUMBER:
+      self->priv->device_number = g_value_get_int (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+
+  }
+}
+
+static void
+gst_bc63x_clock_finalize (GObject * object)
+{
+
+}
+
+static void
+gst_bc63x_clock_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstBc63xClock *self = GST_BC63X_CLOCK(object);
+
+  switch (prop_id) {
+    case PROP_DEVICE_NUMBER:
+      g_value_set_uint (value, self->priv->device_number);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+    
+
+}
+
+gboolean
+gst_bc63x_is_initialized (void)
+{
+  return bc63xOpened;
+}
+
+
+gboolean
+gst_bc63x_init (guint64 clock_id, gchar ** interfaces)
+{
+
+    return 1;
+}
+
+
+GstClock *
+gst_bc63x_clock_new (const gchar * name, guint deviceIndex)
+{
+  GstClock *clock;
+
+  g_return_val_if_fail (deviceIndex <= G_MAXUINT8, NULL);
+
+  if (!bc63xOpened && !gst_bc63x_init (GST_BC63X_CLOCK_ID_NONE, NULL)) {
+    GST_ERROR ("Failed to initialize BC63X");
+    return NULL;
+  }
+
+  clock = g_object_new (GST_TYPE_BC63X_CLOCK, "name", name, "deviceIndex", deviceIndex,
+      NULL);
+
+  /* Clear floating flag */
+  gst_object_ref_sink (clock);
+
+  return clock;
+}
---
+++ sys/bc63xpci/gstbc63xclock.h
@@ -0,0 +1,84 @@
+#ifndef __GST_BC63X_CLOCK_H__
+#define __GST_BC63X_CLOCK_H__
+
+#include <gst/gst.h>
+#include <gst/gstsystemclock.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_BC63X_CLOCK \
+  (gst_bc63x_clock_get_type())
+#define GST_BC63X_CLOCK(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_BC63X_CLOCK,GstBc63xClock))
+#define GST_BC63X_CLOCK_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_BC63X_CLOCK,GstBc63xClockClass))
+#define GST_IS_BC63X_CLOCK(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_BC63X_CLOCK))
+#define GST_IS_BC63X_CLOCK_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_BC63X_CLOCK))
+
+
+typedef struct _GstBc63xClock GstBc63xClock;
+typedef struct _GstBc63xClockClass GstBc63xClockClass;
+typedef struct _GstBc63xClockPrivate GstBc63xClockPrivate;
+
+/**
+ * GstBc63xClock:
+ *
+ * Opaque #GstBc63xClock structure.
+ */
+struct _GstBc63xClock {
+  GstSystemClock clock;
+
+  /*< private >*/
+  GstBc63xClockPrivate *priv;
+  
+
+  gpointer _gst_reserved[GST_PADDING];
+};
+
+/**
+ * GstBc63xClockClass:
+ * @parent_class: parented to #GstSystemClockClass
+ *
+ * Opaque #GstBc63xClockClass structure.
+ */
+struct _GstBc63xClockClass {
+  GstSystemClockClass parent_class;
+
+  /*< private >*/
+  gpointer _gst_reserved[GST_PADDING];
+};
+
+/**
+ * GST_BC63X_CLOCK_ID_NONE:
+ * BC63X clock identification that can be passed to gst_bc63x_init() to
+ * automatically select one based on the MAC address of interfaces
+ */
+#define GST_BC63X_CLOCK_ID_NONE ((guint64) -1)
+
+GST_API
+GType           gst_bc63x_clock_get_type             (void);
+
+GST_API
+gboolean        gst_bc63x_is_supported               (void);
+
+GST_API
+gboolean        gst_bc63x_is_initialized             (void);
+
+GST_API
+gboolean        gst_bc63x_init                       (guint64 clock_id,
+                                                    gchar ** interfaces);
+GST_API
+void            gst_bc63x_deinit                     (void);
+
+GST_API
+GstClock*       gst_bc63x_clock_new                  (const gchar *name,
+                                                    guint deviceIndex);
+
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(GstBc63xClock, gst_object_unref)
+
+G_END_DECLS
+
+#endif /* __GST_BC63X_CLOCK_H__ */
